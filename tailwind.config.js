723dfdd48c92547bcbad7f8f4cc78e6a225519af
kingahmadr <ahmadnromiz@gmail.com>
2024-08-27 15:32:59 +0700

    cadangan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        karla: ["Karla", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+        lora: ["Lora", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
